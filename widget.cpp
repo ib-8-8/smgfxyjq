71d2725b944a9d38c9f7895596c29fc4a21898b0
ib-8-8 <2563656160@qq.com>
2026-04-16 18:45:59 +0800

    Add files via upload

--- widget.cpp
+++ widget.cpp
@@ -2,12 +2,17 @@
 #include "ui_widget.h"
 
 #include <qdebug.h>
+#include<fsmpBeeper.h>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    ui->horizontalSlider->setRange(0,100);
+
+    connect(&myevent,&fsmpEvents::keyPressed,this,&Widget:: pushbutton);
 }
 
 Widget::~Widget()
@@ -32,6 +37,77 @@ void Widget::on_pushButton_clicked()
 
 void Widget::on_pushButton_2_clicked()
 {
-    qDebug()<<"xyjq"<<endl;
+
 }
+
+
+void Widget::on_checkBox_stateChanged(int arg1)
+{
+    if(arg1){
+
+        myled.on(fsmpLeds::LED2);    }
+    else
+    {
+
+        myled.off(fsmpLeds::LED2);
+    }
+}
+
+
+
+void Widget::on_checkBox_2_stateChanged(int arg1)
+{
+    if(arg1){
+
+        mybeeper.setRate(5000);
+        mybeeper.start();
+    }
+    else
+    {
+        mybeeper.stop();
+    }
+}
+
+
+void Widget::on_horizontalSlider_valueChanged(int value)
+{
+    qDebug() << ui->horizontalSlider->value();
+
+    mybeeper.setRate(value);
+    mybeeper.start();
+
+    ui->progressBar->setValue(ui->horizontalSlider->value());
+}
+
+
+void Widget::on_checkBox_3_stateChanged(int arg1)
+{
+    if(arg1){
+
+            myfan.setSpeed(100);
+            myfan.start();
+        }
+        else
+        {
+            myfan.stop();
+        }
+}
+void Widget:: pushbutton(int num){
+
+  switch(num){
+
+  case 1:
+      myled.on(fsmpLeds::LED1);
+      break;
+  case 2:
+      myled.on(fsmpLeds::LED2);
+      break;
+  case 3:
+      myled.on(fsmpLeds::LED3);
+      break;
+  default:
+      break;
+  }
+
+};
 
